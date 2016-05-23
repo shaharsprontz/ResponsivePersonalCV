e0a3c14ee11ff9a89385a7f497f93de90594b2c7
shaharsprontz <shahar@getstocks.com>
2016-05-23 09:45:46 +0300

    Add files via upload

---
+++ CV_Responsive/myscripts.js
@@ -0,0 +1,18 @@
+/**
+ * Created by shahar on 19/05/16.
+ */
+
+
+/**if (screen.width <= 740) {
+    document.getElementById("leftContain").classList.remove("leftContainer");
+}else {
+    document.getElementById("leftContain").className = "leftContainer";
+}**/
+
+
+   /** if (window.innerWidth < 740) {
+        var toBeRemoved = document.getElementById("leftContain").classList;
+        toBeRemoved.remove("leftContainer");
+    } else {
+        toBeRemoved.add("blue");
+    };
